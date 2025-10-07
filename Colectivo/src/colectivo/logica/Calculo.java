3f7b0ddb1f4673e589ef663648bac62cb05a75d2
Francisco Molina <franagus370@gmail.com>
2025-10-07 13:51:07 -0300

    avances en logica (corre)

--- Colectivo/src/colectivo/logica/Calculo.java
+++ Colectivo/src/colectivo/logica/Calculo.java
@@ -24,15 +24,15 @@ public class Calculo {
      * @return Lista de listas de recorridos posibles
      */
     public static List<List<Recorrido>> calcularRecorrido(Parada paradaOrigen, Parada paradaDestino, int diaSemana,
-                                                          LocalTime horaLlegaParada, Map<String, Tramo> tramos) {
+                                                   LocalTime horaLlegaParada, Map<String, Tramo> tramos) {
         List<List<Recorrido>> recorridos = new ArrayList<>();
         List<Recorrido> recorridoActual = new ArrayList<>();
-        buscarRecorridos(paradaOrigen, paradaDestino, tramos, recorridoActual, recorridos);
+        buscarRecorridos(paradaOrigen, paradaDestino, tramos, recorridoActual, recorridos, horaLlegaParada, diaSemana);
         return recorridos;
     }
 
     private static void buscarRecorridos(Parada actual, Parada destino, Map<String, Tramo> tramos,
-                                         List<Recorrido> recorridoActual, List<List<Recorrido>> recorridos) {
+                                         List<Recorrido> recorridoActual, List<List<Recorrido>> recorridos, LocalTime horaLlegaParada, int diaSemana) {
         if (actual.equals(destino)) {
             recorridos.add(new ArrayList<>(recorridoActual));
             return;
@@ -42,14 +42,30 @@ public class Calculo {
             if (i.getInicio().equals(actual) && !recorridoActual.contains(i)) {
                 //defino las paradas a agregar
                 List<Parada> paradasTramo = new ArrayList<>();
-                paradasTramo.add(i.getInicio().ge);
+                paradasTramo.add(i.getInicio());
                 paradasTramo.add(i.getFin());
 
                 //Defino las lineas que hay en las paradas para el recorrido
-                Linea lineaTramo = ;
+                Linea lineaTramo = obtenerLinea(i.getInicio(), i.getFin());
 
+                Recorrido r = new Recorrido(lineaTramo, paradasTramo, horaLlegaParada, i.getTiempo());
+
+                // Agregar recorrido actual, avanzar recursivamente y después quitarlo (backtracking)
+                recorridoActual.add(r);
+                buscarRecorridos(i.getFin(), destino, tramos, recorridoActual, recorridos, horaLlegaParada, diaSemana);
+                recorridoActual.remove(recorridoActual.size() - 1);
             }
         }
+    }
+
+    private static Linea obtenerLinea(Parada inicio, Parada fin) {
+        //La parada de inicio y la de fin comparte una sola linea
+        for(Linea l: inicio.getLineas()){
+            if(fin.getLineas().contains(l)){
+                return l;
+            }
+        }
+        return null;
     }
 
 }
